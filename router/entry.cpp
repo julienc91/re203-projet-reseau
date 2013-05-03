5a3d803de25503c4b28c1a201ed987ff98e89343
maxime.bellier@gmx.fr <mbellier@enseirb-matmeca.fr>
2013-05-03 11:49:24 +0200

    router/entry : entrée de la table de routage

---
+++ router/entry.cpp
@@ -0,0 +1,47 @@
+#include "rout_table.hpp"
+#include <string>
+
+using namespace std;
+
+
+
+Entry::Entry(string name, string nextHop, int dist)
+  : _name(name),
+    _nextHop(nextHop),
+    _dist(dist)
+{
+}
+
+
+Entry::Entry(const Entry *data){
+    _name       = data->_name;
+    _nextHop    = data->_nextHop;
+    _dist       = data->_dist;
+}
+
+string &Entry::name(){
+  return _name;
+}
+
+string Entry::name() const{
+  return _name;
+}
+
+string &Entry::nextHop(){
+  return _nextHop;
+}
+string Entry::nextHop() const{
+  return _nextHop;
+}
+
+int &Entry::dist(){
+  return _dist;
+}
+
+int Entry::dist() const{
+  return _dist;
+}
+
+Client *client(){
+  return _client;
+}
