77fd111edd63c644f095f8561dfeba512dcea06b
maxime.bellier@gmx.fr <mbellier@enseirb-matmeca.fr>
2013-05-13 20:09:50 +0200

    Router: modification de l'affichage de debug.

--- router/event.cpp
+++ router/event.cpp
@@ -3,8 +3,8 @@
 
 extern "C"
 {
-	#include "../common/net.h"
-	#include "../common/messages.h"
+#include "../common/net.h"
+#include "../common/messages.h"
 }
 #include "event.hpp"
 #include "exec.hpp"
@@ -13,54 +13,56 @@ extern Router* glob__router;
 
 void Event::input(network *net, char *buffer)
 {
-	std::cout << "<input event: '" << buffer << "'>" << std::endl;
+  std::cout << "<input event: '" << buffer << "'>" << std::endl;
 
-	if (strcmp(buffer, "quit") == 0)
-	{
-		network__close(net);
-		return;
-	}
+  if (strcmp(buffer, "quit") == 0)
+    {
+      network__close(net);
+      return;
+    }
 
-	network__send(&(net->clients[0]), buffer);
+  network__send(&(net->clients[0]), buffer);
 }
 
 void Event::connect(network *net, Client *c, char *buffer)
 {
-	#ifdef __ROUTER_DEBUG
-	std::cout << "<connection on socket '" << c->sock << "' : '" << buffer << "'>" << std::endl;
-	#endif
-	Messages *m = mess__multiline_parse(buffer);
+#ifdef __ROUTER_DEBUG
+  std::cout << "<connection on socket '" << c->sock << "' : '" << buffer << "'>" << std::endl;
+#endif
+  Messages *m = mess__multiline_parse(buffer);
 
-	if (!m) return;
+  if (!m) return;
 
-	for (unsigned int i = 0; i < m->nb_messages; i++)
-	{
-		if (m->messages[i] != NULL)
-			glob__router->exec->sock_message(m->messages[i], c);
-	}
-	mess__free_messages(&m);
+  for (unsigned int i = 0; i < m->nb_messages; i++)
+    {
+      if (m->messages[i] != NULL)
+	glob__router->exec->sock_message(m->messages[i], c);
+    }
+  mess__free_messages(&m);
 }
 
 void Event::disconnect(network *net, Client *c)
 {
-	std::cout << "<disconnection from socket '" << c->sock << "' of client '"<< c->id <<"'>" << std::endl;
+  std::cout << "<disconnection from socket '" << c->sock << "' of client '"<< c->id <<"'>" << std::endl;
 }
 
 void Event::message(network *net, Client *c, char *buffer)
 {
-	#ifdef __ROUTER_DEBUG
-	std::cout << "<message from '" << c->id << "', socket " << c->sock <<" : '" << buffer << "'>" << std::endl;
-	#endif
-	Messages *m = mess__multiline_parse(buffer);
+  Messages *m = mess__multiline_parse(buffer);
 
-	if (!m) return;
+  if (!m) return;
 
-	for (unsigned int i = 0; i < m->nb_messages; i++)
+  for (unsigned int i = 0; i < m->nb_messages; i++)
+    {
+      if (m->messages[i] != NULL)
 	{
-		if (m->messages[i] != NULL)
-		{
-			glob__router->exec->sock_message(m->messages[i], c);
-		}
+	  std::cout << "<message from '" << c->id 
+		    << "' on socket '" << c->sock << "' : '"
+		    << mess__toString(m->messages[i])
+		    << "'>" << std::endl;
+
+	  glob__router->exec->sock_message(m->messages[i], c);
 	}
-	mess__free_messages(&m);
+    }
+  mess__free_messages(&m);
 }
