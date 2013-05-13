ada6afb8e279b8e4848e2962ec2f0f2595c0a6bf
maxime.bellier@gmx.fr <mbellier@enseirb-matmeca.fr>
2013-05-13 14:50:41 +0200

    Controller : Paramètre en ligne de commande pour charger une topologie. (./controller topo.dot)

--- controller/main.c
+++ controller/main.c
@@ -5,11 +5,32 @@
 
 extern network *net;
 
-int main(void)
+static void usage(char *bin)
 {
+    fprintf(stderr, "%s [<topology>]\n", bin);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc > 2) usage(argv[0]);
+    
     graph__init();
     table__init();
     exec__init();
+    
+    if (argc == 2)
+    {
+        char *command = malloc(sizeof(char) * (strlen(argv[1]) + 7));
+        strcpy(command, "load ");
+        strcat(command, argv[1]);
+        
+        Message *m = mess__parse(command);
+        exec__prompt_message(m);
+        
+        mess__free(&m);
+        free(command);
+    }
 
 	printf("Graph prompt : Ctrl+D pour quitter\n");
 	pthread_t *prompt_th1 = prompt__start(exec__prompt_message);
@@ -17,8 +38,11 @@ int main(void)
 	// * * * * gestion serveur * * * *
 	while(network__is_opened(net))
 	{
-		network__update(net);
+	  //fprintf(stderr, ".");	
+	  //network__update(net);
+	
 	}
+	fprintf(stderr, "exit main loop\n");
 
 	//~ pthread_join(*prompt_th1, NULL);
 	//~ printf("Prompt quitté\n");
