b9f256a84b30b1026bd414f8d77dc28073282987
julienc91 <julien.chaumont91@hotmail.fr>
2013-05-14 23:39:47 +0200

    Patch du contrôleur: fermeture prématurée du prompt

--- controller/main.c
+++ controller/main.c
@@ -16,11 +16,11 @@ extern network *net;
 
 void *network_loop(void *net)
 {
-    // * * * * gestion serveur * * * *
-	while(network__is_opened((network *)net))
-	{
-        network__update((network *)net);
-	}
+     // * * * * gestion serveur * * * *
+     while(network__is_opened((network *)net))
+     {
+	  network__update((network *)net);
+     }
 }
 
 
@@ -31,8 +31,8 @@ void *network_loop(void *net)
 
 static void usage(char *bin)
 {
-    fprintf(stderr, "%s [<topology>]\n", bin);
-    exit(EXIT_FAILURE);
+     fprintf(stderr, "%s [<topology>]\n", bin);
+     exit(EXIT_FAILURE);
 }
 
 /**
@@ -43,53 +43,53 @@ static void usage(char *bin)
  */
 int main(int argc, char **argv)
 {
-    if (argc > 2) usage(argv[0]);
+     if (argc > 2) usage(argv[0]);
     
-    graph__init();
-    table__init();
-    exec__init();
+     graph__init();
+     table__init();
+     exec__init();
     
-    if (argc == 2)
-    {
-        char *command = malloc(sizeof(char) * (strlen(argv[1]) + 7));
-        strcpy(command, "load ");
-        strcat(command, argv[1]);
+     if (argc == 2)
+     {
+	  char *command = malloc(sizeof(char) * (strlen(argv[1]) + 7));
+	  strcpy(command, "load ");
+	  strcat(command, argv[1]);
         
-        Message *m = mess__parse(command);
-        exec__prompt_message(m);
+	  Message *m = mess__parse(command);
+	  exec__prompt_message(m);
         
-        mess__free(&m);
-        free(command);
-    }
+	  mess__free(&m);
+	  free(command);
+     }
 
-	printf("Graph prompt : Ctrl+D pour quitter\n");
-	pthread_t *prompt_th1 = prompt__start(exec__prompt_message);
+     printf("Graph prompt : Ctrl+D pour quitter\n");
+     pthread_t *prompt_th1 = prompt__start(exec__prompt_message);
 	
-    pthread_t network_thread;
-    pthread_create(&network_thread, NULL, network_loop, (void *)net);
+     pthread_t network_thread;
+     pthread_create(&network_thread, NULL, network_loop, (void *)net);
     
-    pthread_join(*prompt_th1, NULL);
+     pthread_join(*prompt_th1, NULL);
     
-    pthread_cancel(network_thread);
+     pthread_cancel(network_thread);
     
-    pthread_join(network_thread, NULL);
+     pthread_join(network_thread, NULL);
 
-    printf("[CONTROLLER] Closing network...\n");
+     printf("[CONTROLLER] Closing network...\n");
 
-	// * * * * fermeture serveur * * * *
-	network__close(net);
-	network__free(net);
+     // * * * * fermeture serveur * * * *
+     network__close(net);
+     network__free(net);
 
-	// Free memory
-	table__delete();
-	graph__delete();
-    free (prompt_th1);
-    free (config);
-    mess__base__free();
+     // Free memory
+     table__delete();
+     graph__delete();
+     free (prompt_th1);
+     free (config);
+     mess__base__free();
     
-    agclose(graph);
+     agclose(graph);
 	
-    printf("[CONTROLLER] Closed.\n");
+     printf("[CONTROLLER] Closed.\n");
     
-	return  0;
+     return  0;
 }
